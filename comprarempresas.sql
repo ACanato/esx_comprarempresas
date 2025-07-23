2150699064e41a35e624e09b6cedb2b364191d37
Canato <67785569+ACanato@users.noreply.github.com>
2025-07-23 20:29:42 +0100

    comprarempresas.sql

--- comprarempresas.sql
+++ comprarempresas.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS empresas (
     id VARCHAR(50) PRIMARY KEY,
     dono VARCHAR(64) DEFAULT '',
     nivel INT DEFAULT 0 NOT NULL,
-    avisos INT NOT NULL DEFAULT 0;
+    avisos INT NOT NULL DEFAULT 0,
+    dinheiro INT DEFAULT 0
 );
